78d755026f678e41adfd9bbbef22581edf5004d8
wangi <lee@leekindness.com>
2025-12-17 20:04:12 +0000

    version which was running for 5 years

--- 26et.pl
+++ 26et.pl
@@ -1,13 +1,57 @@
 #!/usr/bin/env perl
+
 use warnings;
 use strict;
 use Device::TotalConnectComfort;
+use LWP;
+use JSON;
+use Encode;
+use POSIX;
+use DBI;
+
+## get current time, output filename
+my $t     = time;
+my $tfmt  = POSIX::strftime("%m/%d/%Y %H:%M:%S", gmtime($t));
+my $fname = POSIX::strftime("/home/pi/data/%Y%m.csv", gmtime($t));
+my $file_exists = -f $fname ? 1 : 0;
+open my $fh, ">>$fname" or die;
 
 # AUTHENTICATION:
 # environment variable TCC_USERNAME & TCC_PASSWORD
-my ($username, $password) = @ARGV;
+my ($username, $password, $DBUSER, $DBPASS) = @ARGV;
 $username = $ENV{TCC_USERNAME} unless $username;
 $password = $ENV{TCC_PASSWORD} unless $password;
+$DBUSER   = $ENV{DBUSER}       unless $DBUSER;
+$DBUSER   = $ENV{DBPASS}       unless $DBPASS;
+my $DBNAME = 'evohome';
+my $DBHOST = 'localhost';
+
+# connect to db
+my $db = DBI->connect("DBI:Pg:dbname=$DBNAME;host=$DBHOST", $DBUSER, $DBPASS);
+my $query = $db->prepare("INSERT INTO evohome(datetime, temperature, humidity, weather, temp_living, temp_living_target, temp_kitchen, temp_kitchen_target, temp_toilet, temp_toilet_target, temp_utility, temp_utility_target, temp_freya, temp_freya_target, temp_spare, temp_spare_target, temp_landing, temp_landing_target, temp_master, temp_master_target, temp_study, temp_study_target) VALUES(to_timestamp(?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+## get current weather: BBC, Joppa
+my $URL = 'https://weather-broker-cdn.api.bbci.co.uk/en/observation/rss/2645947';
+my $userAgent = LWP::UserAgent->new(keep_alive => 20);
+my $resp = $userAgent->get($URL);
+my $bbc_temperature  = '';
+my $bbc_humidity     = '';
+my $bbc_weatherstate = '';
+foreach ( split "\n", decode('utf-8', $resp->content) )
+{
+	$bbc_temperature = $1 if( /Temperature: ([\d\-]+).C/ );
+	$bbc_humidity    = $1 if( /Humidity: ([\d]+)%/ );
+}
+
+## get current weather
+$URL = 'https://www.metaweather.com/api/location/19344/';
+$resp = $userAgent->get($URL);
+$resp = decode('utf-8', $resp->content());
+my $json = JSON->new();
+my $mw = $json->decode($resp);
+my $temperature  = $mw->{consolidated_weather}->[0]->{the_temp};
+my $humidity     = $mw->{consolidated_weather}->[0]->{humidity};
+my $weatherstate = lc $mw->{consolidated_weather}->[0]->{weather_state_name};
 
 # Log in
 my $cn = Device::TotalConnectComfort->new( username => $username,
@@ -21,17 +65,19 @@ my $location_id = $locations_data->[0]->{locationInfo}->{locationId};
 
 # Get data on the default location
 my $status_data = $cn->get_status($location_id);
-describe_status( $status_data );
-exit;
-
-# Describe status at a given location
-sub describe_status {
-    my $status_data = shift;
-
-    for my $zone ( @{ $status_data->{gateways}->[0]->{temperatureControlSystems}->[0]->{zones} } ) {
-        print "$zone->{name},$zone->{temperatureStatus}->{temperature},$zone->{setpointStatus}->{targetHeatTemperature}\n";
-    }
-
+my %data = ();
+for my $zone ( @{$status_data->{gateways}->[0]->{temperatureControlSystems}->[0]->{zones}} )
+{
+	my $name = lc $zone->{name};
+	$data{$name}{temp}   = $zone->{temperatureStatus}->{temperature};
+	$data{$name}{target} = $zone->{setpointStatus}->{targetHeatTemperature};
 }
 
+print $fh "unixtime,datetime,temperature,humidity,weather,living,living target,kitchen,kitchen target,toilet,toilet target,utility,utility target,freya,freya target,spare,spare target,landing,landing target,master,master target,study,study target\n" if( $file_exists == 0 );
+print $fh "$t,$tfmt,$bbc_temperature,$bbc_humidity,$weatherstate,$data{living}{temp},$data{living}{target},$data{kitchen}{temp},$data{kitchen}{target},$data{toilet}{temp},$data{toilet}{target},$data{utility}{temp},$data{utility}{target},$data{freya}{temp},$data{freya}{target},$data{spare}{temp},$data{spare}{target},$data{landing}{temp},$data{landing}{target},$data{master}{temp},$data{master}{target},$data{study}{temp},$data{study}{target}\n";
+close $fh;
+
+$query->execute($t, $bbc_temperature, $bbc_humidity, $weatherstate, $data{living}{temp}, $data{living}{target}, $data{kitchen}{temp}, $data{kitchen}{target}, $data{toilet}{temp}, $data{toilet}{target}, $data{utility}{temp}, $data{utility}{target}, $data{freya}{temp}, $data{freya}{target}, $data{spare}{temp}, $data{spare}{target}, $data{landing}{temp}, $data{landing}{target}, $data{master}{temp}, $data{master}{target}, $data{study}{temp}, $data{study}{target});
+
+__DATA__
 
